c7a5984cdd7ba6183742e34213db1edb78fbebd0
Daniel Gomes <daniel167t@gmail.com>
2024-09-27 13:40:00 -0300

    Atualizando executavel e args

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const app = express()
 const puppeteer = require('puppeteer')
+require("dotenv").config()
 
 
 app.post('/pdf', async (req, res) => {
     // const pup = async () => {
     // }
     // pup()
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({
+        args: [
+            "--disable-setuid-sandbox",
+            "--no-sandbox",
+            "--single-process",
+            "--no-zygote"
+        ]
+        executablePath: 
+        process.env.NODE_ENV === 'production' 
+            ? process.env.PUPPETTER.EXECUTABLE_PATH
+            : puppeteer.executablePath()
+    })
     const page = await browser.newPage()
     await page.setContent('<p>deu certo</p>')
     const pdf = await page.pdf({format: 'A4', path: 'catalogo.pdf'})
