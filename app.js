d14ec883c13dc709efcf946eb3c3c450aa80dcae
Daniel Gomes <daniel167t@gmail.com>
2024-09-27 14:29:23 -0300

    teste 3

--- app.js
+++ app.js
@@ -5,23 +5,27 @@ require("dotenv").config()
 
 
 app.post('/pdf', async (req, res) => {
+    try {
+        const browser = await puppeteer.launch({
+            executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/google-chrome-stable',
+            headless: true, // Executa o navegador em modo headless
+            args: ['--no-sandbox', '--disable-setuid-sandbox'], // Argumentos recomendados para ambientes como Render
+        });
+        const page = await browser.newPage()
+        await page.setContent('<p>deu certo</p>')
+        const pdf = await page.pdf({format: 'A4', path: 'catalogo.pdf'})
+    
+        await page.close()
+        await browser.close()
+        
+        res.send(pdf)
+    }catch (err){
+        res.json({msg: `Falha ao gerar o boleto, erro ${err}` })
+    }
+})
     // const pup = async () => {
     // }
     // pup()
-    const browser = await puppeteer.launch({
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/google-chrome-stable',
-        headless: true, // Executa o navegador em modo headless
-        args: ['--no-sandbox', '--disable-setuid-sandbox'], // Argumentos recomendados para ambientes como Render
-    });
-    const page = await browser.newPage()
-    await page.setContent('<p>deu certo</p>')
-    const pdf = await page.pdf({format: 'A4', path: 'catalogo.pdf'})
-
-    await page.close()
-    await browser.close()
-    
-    res.send(pdf)
-})
 
 app.listen(3031, () => {
     console.log('Server Teste');
